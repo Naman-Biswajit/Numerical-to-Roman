c2113c9bcdd71ce3e9642cb552c8f7a72ed96eb1
Naman Biswajit <62687888+Naman-Biswajit@users.noreply.github.com>
2020-12-18 22:59:59 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+basic = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL',
+              50: 'L', 90: 'XC', 100: 'C', 400: 'XD', 500: 'D', 900: 'CM', 1000: 'M'}
+
+num = int(input("Enter a number: "))
+
+index = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+for x in index:
+    if num != 0:
+        quotient = num // x
+ 
+        if quotient != 0:
+            for y in range(quotient):
+                print(basic[x], end="")
+ 
+        num = num % x
